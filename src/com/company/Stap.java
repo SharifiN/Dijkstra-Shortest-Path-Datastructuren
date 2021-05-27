b407498acec60dc5c4714ec9e68df38bdad4bdd3
N.Sharifi <nasser.sharifi@student.hu.nl>
2021-05-27 15:46:04 +0200

    In deze class gaan we stap bekijken een bestestming of reis heeft 1 of meer stappen met deze gegevens gaan we kijken of de alogrimtme goed werkt of niet.

---
+++ src/com/company/Stap.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Stap {
+    private String PuntA;
+    private String PuntB;
+
+    public Stap(String puntA, String puntB) {
+        this. PuntA = puntA;
+        this. PuntB = puntB;
+    }
+
+    public String getPuntA() {
+        return PuntA;
+    }
+
+    public void setPuntA(String puntA) {
+        PuntA = puntA;
+    }
+
+    public String getPuntB() {
+        return PuntB;
+    }
+
+    public void setPuntB(String puntB) {
+        PuntB = puntB;
+    }
+}
+
+
