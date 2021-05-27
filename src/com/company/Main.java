aef63862d56d4bcbea0bcc33ec21ac7adabca20f
N.Sharifi <nasser.sharifi@student.hu.nl>
2021-05-27 15:40:42 +0200

    In de main wordt alle functie binnen de classen aangeropenn om interfacen.

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stap Amsterdam_denhaag =new Stap("Amsterda","Den-Haag");
+        Rit Amsterdam_denhaag1 = new Rit("Amsterdam","Den-Haag", 120);
+        Vlucht Amsterdam_denhaag2 = new Vlucht("Amsterda","Den-Haag",'850',30);
+        Treinrit Amsterdam_denhaag3 = new Treinrit("Amsterdam","Denh-Haag",'60');
+
+        Stap Newyork =new Stap("Amsterda","Den-Haag");
+        Rit Amsterdam_denhaag1 = new Rit("Amsterdam","Den-Haag", 120);
+        Vlucht Amsterdam_denhaag2 = new Vlucht("Amsterda","denhaag",'850',30);
+        Treinrit Amsterdam_denhaag3 = new Treinrit("Amsterdam","Denh-Haag",'60');
+
+
+
+
+
+
+    }
+}
