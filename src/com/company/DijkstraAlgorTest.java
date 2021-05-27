229cdea0587db5a41e156096fd3566206a407d9d
N.Sharifi <nasser.sharifi@student.hu.nl>
2021-05-27 17:02:21 +0200

    Hier wordt unit testen gemaakt en wordt gestest.

---
+++ src/com/company/DijkstraAlgorTest.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DijkstraAlgorTest {
+
+    @org.junit.jupiter.api.Test
+    void dijkstar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBeginpunt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setBeginpunt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAfstand() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAfstand() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEindpunt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEindpunt() {
+    }
+}
