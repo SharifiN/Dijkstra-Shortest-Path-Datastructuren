edca4b755e36f61a7e3f236e24f06f72b661c04b
N.Sharifi <nasser.sharifi@student.hu.nl>
2021-05-27 15:38:10 +0200

    In deze documen is Dijkstra's algorithm bijgewerkt waaronder valt 2 variabelen graph en afstand tussen 2 punten.

---
+++ src/com/company/DijkstraAlgor.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class DijkstraAlgor {
+    private String Graph;
+    private int Afstand;
+
+    public DijkstraAlgor(String graph, int afstand) {
+        Graph = graph;
+        Afstand = afstand;
+    }
+    public void Dijkstar(String graph, int afstand){
+
+
+        
+    }
+
+    public String getGraph() {
+        return Graph;
+    }
+
+    public void setGraph(String graph) {
+        Graph = graph;
+    }
+
+    public int getAfstand() {
+        return Afstand;
+    }
+
+    public void setAfstand(int afstand) {
+        Afstand = afstand;
+    }
+}
+
